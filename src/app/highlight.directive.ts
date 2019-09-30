163bbc129f0963299136caf94fde5852c5c472cb
alexmwaura <alexmwaura43@gmail.com>
2019-09-30 13:39:49 +0300

    to change color on mouse enter

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -8,7 +8,7 @@ export class HighlightDirective {
   constructor(private el: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('green');
+    this.highlight('teal');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
